431030105509d7f5665bf4ddac5f60778594b16d
UZGNG <87087961+UZGNG@users.noreply.github.com>
2022-01-26 20:50:46 -0600

    updated

--- situations.js
+++ situations.js
@@ -6,6 +6,8 @@ add a function for when each button is clicked
 
 /* WIN RESULTS */
 
+//
+
 document.getElementById("buttonTryDoor").addEventListener("click", function tryDoorResult() {
     document.getElementById("situationText").innerHTML = "After trying the door, it surprisingly just opened. Of course, as you step out, you're spotted by a guard who is wearing very old armor and only has a sword as a weapon. You notice that there is a sword lying around, but you also believe that you can use the martial arts to get yourself out of this mess. What will you do?"
     document.getElementById("buttonTryDoor").style.display = "none"
@@ -35,19 +37,46 @@ document.getElementById("buttonTryStairs").addEventListener("click", function tr
     document.getElementById("buttonTryBridge").style.display = "inline"
     document.getElementById("buttonTryTech").style.display = "inline"
 });
+
+//ENDINGS METHOD 1
+
 document.getElementById("buttonTryBathroom").addEventListener("click", function tryBathroomResult() {
-    document.getElementById("situationText").innerHTML = "You sprint into the nearest restroom. You quickly check your surroundings and see a hole in the ground. You hesitate because that's disgusting. However you find it a necesary evil just in case it helps you find a way home. You peak over the edge to find... a portal? You assume that this is your shot, your only way out of this time period. You fit yourself into this very small hole. You wake up in the middle of the street. You scramble to your feet and go read a sign that says 'France, 1792'. You dont realize the significance of that date until you hear a loud thump in the background. You turn around to watch as a bloody blade is being raised. That is a guillotine, and YOU are stuck during the bloodiest time of the French Revolution."
+    var frEnding = "You wake up in the middle of the street. You scramble to your feet and go read a sign that says 'France, 1792'. You dont realize the significance of that date until you hear a loud thump in the background. You turn around to watch as a bloody blade is being raised. That is a guillotine, and YOU are stuck during the bloodiest time of the French Revolution."
+    var shipEnding = "You wake up in the middle of a ship. As you observe your surroundings, you notice this is an old wooden ship. You find yourself surrounded by pirates. They accept you as one of their own. Your new name is Time Beard, and you're starting your next adventure to get back to your proper time."
+    var correctEnding = "You wake up back in your room. You get up and look around just to confirm that you are indeed back in your time period. You deduce that you're back in your time period because your room still looks like a mess after the party that transported you in the first place. You did it!"
+
+    var endings = [frEnding, shipEnding, correctEnding];
+    var endingIdx = Math.floor(Math.random()*endings.length);
+    var ending = endings[endingIdx];
+    var house = document.getElementById('house')
+    var flag = document.getElementById('flag')
+    var ship = document.getElementById('ship')
+    var bckgrndImg = document.getElementById('scroll')
+    if (ending == shipEnding) {
+        document.getElementById('situationText').style.color = "white"
+        document.getElementById("playAgainButton").style.color = "white"
+    
+    }
+    if (ending == frEnding) {
+        bckgrndImg.style.display = 'none'
+        flag.style.display = 'inline'
+        document.getElementById('situationText').style.color = "gold"
+        document.getElementById("playAgainButton").style.color = "gold"
+    }
+    if (ending == shipEnding) {
+        bckgrndImg.style.display = 'none'
+        ship.style.display = 'inline'
+    }
+    if (ending == correctEnding) {
+        bckgrndImg.style.display = 'none'
+        house.style.display = 'inline'
+    }
+    document.getElementById("situationText").innerHTML = `You sprint into the nearest restroom. You quickly check your surroundings and see a hole in the ground. You hesitate because that's disgusting. However you find it a necesary evil just in case it helps you find a way home. You peak over the edge to find... a portal? You assume that this is your shot, your only way out of this time period. You fit yourself into this very small hole. ${ending}`;
     document.getElementById("buttonTryBathroom").style.display = "none"
     document.getElementById("buttonTryBridge").style.display = "none"
     document.getElementById("buttonTryTech").style.display = "none"
     document.getElementById("playAgainButton").style.display = "inline"
-
-
 });
-
-
-
-
 
 /* FAIL RESULTS */
 
@@ -58,21 +87,34 @@ document.getElementById("buttonTasteKoolaid").addEventListener("click", function
     document.getElementById("situationText").innerHTML = "My only question is why? Why would you do that? NO! It was not Koolaid! Was Koolaid even a thing in this time period? You know what? Simply because you acted so stupidly this is a fail. YOU LOSE."
     document.getElementById("retryButton").style.display = "inline"
 });
+
 document.getElementById("buttonRunIntoStone").addEventListener("click", function tryWallResult() {
     document.getElementById("buttonTryDoor").style.display = "none"
     document.getElementById("buttonTasteKoolaid").style.display = "none"
     document.getElementById("buttonRunIntoStone").style.display = "none"
     document.getElementById("situationText").innerHTML = "Congratulations! After ramming your head into the stone wall multile times you died due to brain damage. Nobody bothered to clean you up either. YOU LOSE."
     document.getElementById("retryButton").style.display = "inline"
-
 });
+
 document.getElementById("buttonTryMartialArts").addEventListener("click", function tryMartialArtsResult() {
     document.getElementById("buttonTryMartialArts").style.display = "none"
     document.getElementById("buttonUseSword").style.display = "none"
     document.getElementById("buttonCharge").style.display = "none"
-    document.getElementById("situationText").innerHTML = "You attempt to hit the guard with a reverse dragon kick combo. you leap high into the air, and land flat on your face. The gaurd stabs you while you're face down on the floor. YOU LOSE."
-    document.getElementById("retryButton").style.display = "inline"
-
+    var maFail = "and land flat on your face. The gaurd stabs you while you're face down on the floor. YOU LOSE."
+    var maSuccess = "and you actually manage to hit the guard with this fiery combo. He falls and you're victorious. You notice a few escape routes such as a barred window, a vent on the floor, and a staircase made of stone. What is your next move?"
+    var maArray = [maFail, maFail, maFail, maFail, maFail, maFail, maFail, maFail, maSuccess, maSuccess]
+    var maIdx = Math.floor(Math.random() * maArray.length);
+    var ma = maArray[maIdx];
+    document.getElementById("situationText").innerHTML = `You attempt to hit the guard with a reverse dragon kick combo. You leap high into the air, ${ma}`
+    if (ma == maSuccess) {
+        document.getElementById("buttonTryStairs").style.display = "inline"
+        document.getElementById("buttonTryVent").style.display = "inline"
+        document.getElementById("buttonTryWindow").style.display = "inline"
+        document.getElementById("retryButton").style.display = "none"
+    }
+    else{
+        document.getElementById("retryButton").style.display = "inline"
+    }
 });
 
 document.getElementById("buttonCharge").addEventListener("click", function tryMartialArtsResult() {
@@ -81,13 +123,14 @@ document.getElementById("buttonCharge").addEventListener("click", function tryMa
     document.getElementById("buttonCharge").style.display = "none"
     document.getElementById("situationText").innerHTML = "You run at a whopping 5 miles per hour at the guard. The guard stares at you as you run. Just before you reach him he very slowly side steps. You miss, and with all of your momentum forward, you run straight into the wall head first. You die due to the blunt force of the wall. The guard also died due to the lack of brain cells after witnessing your super dumb escape attempt.  YOU LOSE."
     document.getElementById("retryButton").style.display = "inline"
-
 });
 
 document.getElementById("buttonTryVent").addEventListener("click", function tryVentResult() {
     document.getElementById("buttonTryStairs").style.display = "none"
     document.getElementById("buttonTryVent").style.display = "none"
     document.getElementById("buttonTryWindow").style.display = "none"
+
+    
     document.getElementById("situationText").innerHTML = "You swiftly leap into the vent and pop out on the other side. You have now found yourself in the barracks, and the guards inside very quickly deduce that you are indeed the imposter. They kill you on the spot. YOU LOSE!"
     document.getElementById("retryButton").style.display = "inline"
 });
@@ -130,4 +173,9 @@ document.getElementById("playAgainButton").addEventListener("click", function ag
     document.getElementById("buttonTasteKoolaid").style.display = "inline"
     document.getElementById("buttonRunIntoStone").style.display = "inline"
     document.getElementById("playAgainButton").style.display = "none"
+    document.getElementById("scroll").style.display = "inline"
+    document.getElementById("house").style.display = "none"
+    document.getElementById("ship").style.display = "none"
+    document.getElementById("flag").style.display = "none"
+    document.getElementById('situationText').style.color = "black"
 });
